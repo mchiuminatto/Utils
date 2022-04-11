48a253da1fb1ddd7e525ccc9483c11577a9438a6
Marcello Chiuminatto <mchiuminatto@gmail.com>
2022-04-11 16:03:37 -0300

    2022-04-11 Added match and next to, Added search for reversed order arrays

--- BinarySearch.py
+++ BinarySearch.py
@@ -53,7 +53,7 @@ class BinarySearch:
             # print('Left half', A[low: mid])
             return self.binary_search_exact_asc_rec(x, A, left, mid)
 
-    def binary_search_exact_asc_iter_any(self, x: any, A: list) -> int:
+    def find_exact_asc_iter_any(self, x: any, A: list) -> int:
         """
         Finds element x in array A using binary search, returning any found (not necessarily the
         first or last one found
@@ -73,52 +73,165 @@ class BinarySearch:
             mid = left + (right - left) // 2 # (high + low) can lead to overflow
             if x == A[mid]:
                 return mid  # element found
-            if  x > A[mid]:
+            elif  x > A[mid]:
                 left = mid + 1  # element could be on the right side
             else:
                 right = mid - 1  # element could be on the left side
 
         return -1
 
-
-    def binary_search_exclo_asc_rec(self, x: any, A: list, low: int, high: int) -> (bool, int):
-
+    def find_exact_asc_iter_first(self, x: any, A: list) -> int:
         """
-        Recursive implementation of binary search, for exact match and closer match if not found
-        on an ascending ordered array
+        Finds the first occurrence of x in the array, in case there are more than one
 
         Args:
-            x: value to be found
-            A: array where to search the value for
-            low: low boundary of the array sub-array where to search for
-            high: high boundary of the sub-array to search for
+            x: Value to be found
+            A: Array where to search the value for.
 
         Returns:
-            found: Boolean indicating whether the value was found or not (True/False)
-            index: If the value was found, is the index where the value was found otherwise is the
-                   index value at right (or the first greater value).
-
-                   Special Cases:
-                   - if the value is lower than the first value of the list, index = 0.
-                   - if the value is higher than the latest in the list, index = len(A)
+            i: Array index where the value was found
 
         """
-        print('In:', "low=", low, "high=", high, "A[low:high]", A[low:high + 1])
-        mid = (low + high) // 2
-        print("mid=", mid, "A=", A, "A[mid]=", A[mid])
 
-        if x == A[mid]:
-            # value found at mid position
-            return (True, mid)
-        if high == low:
-            # value was not found and no more split is possible
-            return (False, high)
-        # further splitting is possible so we decide where to split at
-        if x > A[mid]:
-            # search on right half
-            print('Right half', A[mid + 1: high + 1])
-            return self.binary_search_exclo_asc_rec(x, A, mid + 1, high)
-        elif x < A[mid]:
-            # search on the left half
-            print('Left half', A[low: mid])
-            return self.binary_search_exclo_asc_rec(x, A, low, mid)
+        _left = 0
+        _right = len(A) - 1
+        _result = -1
+        while _left <= _right:
+            mid = _left + (_right - _left) // 2  # (high + low) can lead to overflow
+            if x == A[mid]:
+                _result = mid
+                _right = mid - 1
+            elif x > A[mid]:
+                _left = mid + 1  # element could be on the right side
+            else:
+                _right = mid - 1  # element could be on the left side
+
+        return _result
+
+    def find_exact_asc_iter_last(self, x: any, A: list) -> int:
+        """
+        Finds the last occurrence of x in the array, in case it there is more than one
+
+        Args:
+            x: Value to be found
+            A: Array where to search the value for.
+
+        Returns:
+            i: Array index where the value was found
+
+        """
+
+        _left = 0
+        _right = len(A) - 1
+        _result = -1
+        while _left <= _right:
+            mid = _left + (_right - _left) // 2  # (high + low) can lead to overflow
+            # print("Left ", _left, "Right ", _right, "Mid ", mid, "A[mid]", A[mid], "Result ", _result)
+            if x == A[mid]:
+                _result = mid
+                _left = mid + 1
+            elif x > A[mid]:
+                _left = mid + 1  # element could be on the right side
+            else:
+                _right = mid - 1  # element could be on the left side
+
+        return _result
+
+    def count_instances(self, x: any, A: list)->(int, int, int):
+        """
+        Count instances of x found in A
+
+        Args:
+            x: value to be found and counted
+            A: Array where to search in
+
+        Returns:
+
+            n: number is instances, -1 if none is found.
+            first: first position where x was found
+            last: last position where x was found
+
+        """
+        _first = self.find_exact_asc_iter_first(x, A)
+        if _first == -1:
+            return 0, -1, -1
+        else:
+            _last = self.find_exact_asc_iter_last(x, A)
+        return _last - _first + 1, _first, _last
+
+    def find_next_match(self, x: any, A: list)-> (bool, int):
+
+        """
+        Performs a search of x in A. If found, returns True and a position
+        whe it was found. If not found, returns False and the position where should
+        be inserted.
+
+        Args:
+            x: Value to search for
+            A: List where to search on
+
+        Returns:
+
+            result: Boolean indicating True if value is found, and False if the value is ot found.
+                 n: Position where the value was found (result = True) or position where the value
+                    should be inserted (result = False)
+
+        """
+
+        left: int = 0
+        right: int = len(A) - 1
+        result: bool = False
+        mid: int = -1
+
+        if x < A[left]: return (result, 0)
+        if x > A[right]: return (result, len(A))
+
+        while (left <= right):
+            mid = right - (right - left) // 2
+            if x == A[mid]:
+                result = True
+                break
+            elif x < A[mid]:
+                right = mid - 1
+            else:
+                left =mid + 1
+
+        return (result, mid)
+
+    def find_next_match_desc(self, x: any, A: list)-> (bool, int):
+
+        """
+        Performs a search of x in A. If found, returns True and a position
+        whe it was found. If not found, returns False and the position where should
+        be inserted.
+
+        Args:
+            x: Value to search for
+            A: List where to search on
+
+        Returns:
+
+            result: Boolean indicating True if value is found, and False if the value is ot found.
+                 n: Position where the value was found (result = True) or position where the value
+                    should be inserted (result = False)
+
+        """
+
+        left: int = 0
+        right: int = len(A) - 1
+        mid: int = -1
+
+        if x > A[left]: return (False, 0)
+        if x < A[right]: return (False, len(A))
+
+        while (left <= right):
+            mid = right - (right - left) // 2
+            if x == A[mid]:
+                return (True, mid)
+            elif x < A[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return (False, mid + 1)
+
--- tests/BinarySearch_tests.py
+++ tests/BinarySearch_tests.py
@@ -2,6 +2,7 @@ from Test.TestBase import TestBase
 from Utils.BinarySearch import BinarySearch
 import numpy as np
 
+
 class Tests(TestBase):
 
     def test_case_1(self):
@@ -13,9 +14,9 @@ class Tests(TestBase):
         """
         _bs = BinarySearch()
 
-        A = [1, 2, 3, 5, 7, 9, 12, 20]
+        a = [1, 2, 3, 5, 7, 9, 12, 20]
         x = 9
-        _res = _bs.binary_search_exact_asc_rec(x, A, 0, len(A)-1)
+        _res = _bs.binary_search_exact_asc_rec(x, a, 0, len(a)-1)
         if _res == 5: return True
 
     def test_case_2(self):
@@ -78,7 +79,6 @@ class Tests(TestBase):
                 print("Error ", _res, A.index(x))
                 return False
 
-
     def test_case_10(self):
         """
         Base case: search for an existing value in a small array exact mach
@@ -90,7 +90,7 @@ class Tests(TestBase):
 
         A = [1, 2, 3, 5, 7, 9, 12, 20]
         x = 9
-        _res = _bs.binary_search_exact_asc_iter_any(x, A)
+        _res = _bs.find_exact_asc_iter_any(x, A)
         if _res == 5: return True
 
         return False
@@ -107,7 +107,7 @@ class Tests(TestBase):
 
         A = [1, 2, 3, 5, 7, 9, 12, 20]
         x = -3
-        _res = _bs.binary_search_exact_asc_iter_any(x, A)
+        _res = _bs.find_exact_asc_iter_any(x, A)
         if _res == -1: return True
 
     def test_case_12(self):
@@ -122,7 +122,7 @@ class Tests(TestBase):
 
         A = [1, 2, 3, 5, 7, 9, 12, 20]
         x = 50
-        _res = _bs.binary_search_exact_asc_iter_any(x, A)
+        _res = _bs.find_exact_asc_iter_any(x, A)
         if _res == -1: return True
 
     def test_case_13(self):
@@ -143,7 +143,7 @@ class Tests(TestBase):
         search_for = list(np.random.randint(-500, 1500, 100000))
         _check = True
         for x in search_for:
-            _res = _bs.binary_search_exact_asc_iter_any(x, A)
+            _res = _bs.find_exact_asc_iter_any(x, A)
             if _res == -1:
                 try:
                     A.index(x)
@@ -184,7 +184,7 @@ class Tests(TestBase):
         search_for = set(A).intersection(set(B))
         _check = True
         for x in search_for:
-            _res = _bs.binary_search_exact_asc_iter_any(x, A)
+            _res = _bs.find_exact_asc_iter_any(x, A)
             if _res == -1:
                 try:
                     A.index(x)
@@ -206,7 +206,6 @@ class Tests(TestBase):
 
         return _check and _test_2 and _test_3
 
-
     def test_case_15(self):
         """
         Massive test, A set in in range 0 to 1000
@@ -226,7 +225,7 @@ class Tests(TestBase):
         search_for = set(B).difference(set(A))
         _check = True
         for x in search_for:
-            _res = _bs.binary_search_exact_asc_iter_any(x, A)
+            _res = _bs.find_exact_asc_iter_any(x, A)
             if _res == -1:
                 try:
                     A.index(x)
@@ -248,7 +247,410 @@ class Tests(TestBase):
 
         return _check and _test_2 and _test_3
 
+    def test_case_20(self):
+        """
+        Finds the first occurrence of 5
+        Returns:
+            2
+
+        """
+        _bs = BinarySearch()
+
+        A = [1, 2, 5, 5, 5, 6, 7, 9, 12, 20]
+        x = 5
+        _res = _bs.find_exact_asc_iter_first(x, A)
+        if _res == 2: return True
+
+    def test_case_21(self):
+        """
+        Finds the first occurrence of 9
+        Returns:
+            7
+
+        """
+        _bs = BinarySearch()
+
+        A = [1, 2, 5, 5, 5, 6, 7, 9, 9, 20]
+        x = 9
+        _res = _bs.find_exact_asc_iter_first(x, A)
+        if _res == 7: return True
+
+    def test_case_22(self):
+        """
+        Finds the first occurrence of 9
+        Returns:
+            5
+
+        """
+        _bs = BinarySearch()
+
+        A = [1, 2, 5, 6, 7, 9, 12, 15, 18, 20]
+        x = 9
+        _res = _bs.find_exact_asc_iter_first(x, A)
+        if _res == 5: return True
+
+    def test_case_23(self):
+        """
+        Searches for a no existing element, greater then the right most
+        Returns:
+            -1
+
+        """
+        _bs = BinarySearch()
+
+        A = [1, 2, 5, 5, 5, 6, 7, 9, 9, 20]
+        x = 25
+        _res = _bs.find_exact_asc_iter_first(x, A)
+        if _res == -1: return True
+
+    def test_case_24(self):
+        """
+        Searches for a no existing element, lower than the leftmost
+        Returns:
+            -1
+
+        """
+        _bs = BinarySearch()
+
+        A = [1, 2, 5, 5, 5, 6, 7, 9, 9, 20]
+        x = -10
+        _res = _bs.find_exact_asc_iter_first(x, A)
+        if _res == -1: return True
+
+    def test_case_30(self):
+        """
+        Finds the first occurrence of 5
+        Returns:
+            2
+
+        """
+        _bs = BinarySearch()
+
+        A = [1, 2, 5, 5, 5, 6, 7, 9, 12, 20]
+        x = 5
+        _res = _bs.find_exact_asc_iter_last(x, A)
+        if _res == 4: return True
+
+    def test_case_31(self):
+        """
+        Finds the last occurrence of 9
+        Returns:
+            7
+
+        """
+        _bs = BinarySearch()
+
+        A = [1, 2, 5, 5, 5, 6, 7, 9, 9, 20]
+        x = 9
+        _res = _bs.find_exact_asc_iter_last(x, A)
+        print(_res)
+        if _res == 8: return True
+
+    def test_case_32(self):
+        """
+        Finds the first occurrence 2, with a block of 2's at the leftmost side
+
+        Returns:
+            5
+
+        """
+        _bs = BinarySearch()
+
+        A = [2, 2,  5, 6, 7, 9, 12, 15, 18, 20]
+        x = 2
+        _res = _bs.find_exact_asc_iter_first(x, A)
+        if _res == 0: return True
+
+    def test_case_33(self):
+        """
+        Finds the last occurrence 20, with a block of 20's at the rightmost side
+
+        Returns:
+            5
+
+        """
+        _bs = BinarySearch()
+
+        A = [2, 2,  5, 6, 7, 9, 12, 15, 18, 20, 20, 20]
+        x = 20
+        _res = _bs.find_exact_asc_iter_last(x, A)
+        if _res == 11: return True
+
+    def test_case_40(self):
+        """
+        Finds the number of 5's in the array
+
+        Returns:
+            4,2,5
+
+        """
+        _bs = BinarySearch()
+
+        A = [2, 2,  5, 5, 5, 5, 6, 7, 9, 12, 15, 18, 20, 20, 20]
+        x = 5
+        _res = _bs.count_instances(x, A)
+
+        _test_1 = _res[0] == 4
+        _test_2 = _res[1] == 2
+        _test_3 = _res[2] == 5
+
+        return _test_1 and _test_2 and _test_3
+
+    def test_case_41(self):
+        """
+        Finds the number of 2's in the array
+
+        Returns:
+            2, 0, 1
+
+        """
+        _bs = BinarySearch()
+
+        A = [2, 2,  5, 5, 5, 5, 6, 7, 9, 12, 15, 18, 20, 20, 20]
+        x = 2
+        _res = _bs.count_instances(x, A)
+
+        _test_1 = _res[0] == 2
+        _test_2 = _res[1] == 0
+        _test_3 = _res[2] == 1
+
+        return _test_1 and _test_2 and _test_3
+
+    def test_case_42(self):
+        """
+        Finds the number of 20's in the array
+
+        Returns:
+            2, 0, 1
+
+        """
+        _bs = BinarySearch()
+
+        A = [2, 2,  5, 5, 5, 5, 6, 7, 9, 12, 15, 18, 20, 20, 20]
+        x = 20
+        _res = _bs.count_instances(x, A)
+
+        _test_1 = _res[0] == 3
+        _test_2 = _res[1] == 12
+        _test_3 = _res[2] == 14
+
+        return _test_1 and _test_2 and _test_3
+
+    def test_case_43(self):
+        """
+        Try to find a non-existing value in the array A
+
+        Returns:
+            0, -1, -1
+
+        """
+        _bs = BinarySearch()
+
+        A = [2, 2,  5, 5, 5, 5, 6, 7, 9, 12, 15, 18, 20, 20, 20]
+        x = 40
+        _res = _bs.count_instances(x, A)
+
+        print(_res)
+        _test_1 = _res[0] == 0
+        _test_2 = _res[1] == -1
+        _test_3 = _res[2] == -1
+
+        return _test_1 and _test_2 and _test_3
+
+    def test_case_44(self):
+        """
+        Try to find a non-existing value in the array A
+
+        Returns:
+            0, -1, -1
+
+        """
+        _bs = BinarySearch()
+
+        A = [2, 2,  5, 5, 5, 5, 6, 7, 9, 12, 15, 18, 20, 20, 20]
+        x = 8
+        _res = _bs.count_instances(x, A)
+
+        _test_1 = _res[0] == 0
+        _test_2 = _res[1] == -1
+        _test_3 = _res[2] == -1
+
+        return _test_1 and _test_2 and _test_3
+
+    def test_case_50(self):
+        """
+        Match next: case 1: normal case, value within the array range
+
+        Returns:
+            False, 8
+
+        """
+        _bs = BinarySearch()
+
+        A = [2, 2, 5, 5, 5, 5, 6, 7, 9, 12, 15, 18, 20, 20, 20]
+        x = 19
+        _res = _bs.find_next_match(x, A)
+        _i = _res[1]
+
+        print(_res, A[_i-1], x, A[_i]  )
+
+        _test_1 = False
+        if not _res[0]:
+
+            _test_1 = (x > A[_i-1]) and (x < A[_i])
+
+
+
+        return _test_1
+
+    def test_case_51(self):
+        """
+        Match next: case 2: value is found
+
+        Returns:
+            False, 8
+
+        """
+        _bs = BinarySearch()
+
+        A = [2, 2, 5, 5, 5, 5, 6, 7, 9, 12, 15, 18, 20, 20, 20]
+        x = 5
+        _res = _bs.find_next_match(x, A)
+
+        print(_res)
+        _test_1 = A[_res[1]] == x
+
+        return _test_1
+
+    def test_case_52(self):
+        """
+        Match next: special case: value lower than first element of a.
+
+        Returns:
+            False, 8
+
+        """
+        _bs = BinarySearch()
+
+        A = [2, 2, 5, 5, 5, 5, 6, 7, 9, 12, 15, 18, 20, 20, 20]
+        x = 1
+        _res = _bs.find_next_match(x, A)
+
+        print(_res)
+        _test_1 = _res[1] == 0
+
+        return _test_1
+
+    def test_case_53(self):
+        """
+        Match next: special case: value lower than first element of a.
+
+        Returns:
+            False, 8
+
+        """
+        _bs = BinarySearch()
+
+        A = [2, 2, 5, 5, 5, 5, 6, 7, 9, 12, 15, 18, 20, 20, 20]
+        x = 30
+        _res = _bs.find_next_match(x, A)
+
+        print(_res)
+        _test_1 = _res[1] == len(A)
+
+        return _test_1
+
+    # region descending arrays match and next
+
+    def test_case_60(self):
+        """
+        Match next sorted descending :
+        Normal case: value is missing but in the range of A's values
+
+        Returns:
+            False, 8
+
+        """
+        _bs = BinarySearch()
+
+        A = [15, 14, 12, 12, 12, 8, 8, 7, 6, 3, 3]
+        x = 10
+        _res = _bs.find_next_match_desc(x, A)
+
+        _i = _res[1]
+        print(_i, A[_i-1], x, A[_i])
+        _test_1 = (A[_i] < x) and  (A[_i] > x)
+
+        return _test_1
+
+    def test_case_61(self):
+        """
+        Match next sorted descending :
+        Normal case: value is present
+
+        Returns:
+            False, 8
+
+        """
+        _bs = BinarySearch()
+
+        A = [15, 14, 12, 12, 12, 8, 8, 7, 6, 3, 3]
+        x = 8
+        _res = _bs.find_next_match_desc(x, A)
+
+        _i = _res[1]
+        print(_i, A[_i-1], x, A[_i])
+        _test_1 = A[_i] == x
+
+        return _test_1
+
+    def test_case_62(self):
+        """
+        Match next sorted descending :
+        Special case: value is missing and lower than rightmost value
+
+        Returns:
+            False, len(A)
+
+        """
+        _bs = BinarySearch()
+
+        A = [15, 14, 12, 12, 12, 8, 8, 7, 6, 3, 3]
+        x = 1
+        _res = _bs.find_next_match_desc(x, A)
+
+        _i = _res[1]
+
+        _test_1 = _i == len(A)
+        # print(_res)
+
+        return _test_1
+
+    def test_case_63(self):
+        """
+        Match next sorted descending :
+        Special case: value is missing and higher than leftmost value
+
+        Returns:
+            False, 0
+
+        """
+        _bs = BinarySearch()
+
+        A = [15, 14, 12, 12, 12, 8, 8, 7, 6, 3, 3]
+        x = 20
+        _res = _bs.find_next_match_desc(x, A)
+
+        _i = _res[1]
+
+        _test_1 = _i == 0
+        # print(_res)
+
+        return _test_1
+
+    #endregion
 
 if __name__ == "__main__":
     _test = Tests()
-    _test.run([15])
+    #_test.run([10,11,12,13,14, 15, 20,21,22,23,24, 30, 31, 32, 33, 40, 41, 42, 43, 44])
+    _test.run([63])
